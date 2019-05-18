9dd75a3ba8d92cf2350a58efdb36945c07be49c0
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2019-05-18 22:16:24 +0530

    chore: Add Sass and transplie via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-function defaultTask(cb) {
-    console.log("Gulp here!!");
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { series, src, dest } = require('gulp');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+
+
+function cssClean() {
+    return src('css/**/*.css', {read: false})
+    .pipe(clean());
+}
+
+function cssTranspile(cb) {
+    return src("sass/**/*.scss")
+    .pipe(sass()).on("error", sass.logError)
+    .pipe(dest("css"));
+}
+
+exports.cssTranspile = cssTranspile;
+exports.default = series(cssClean, cssTranspile);
