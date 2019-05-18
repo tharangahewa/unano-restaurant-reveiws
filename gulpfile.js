678281440c846806071945068343a1afc981ae21
Tharanga Hewavithana <tharanga.hewavithana@cinglevue.com>
2019-05-18 22:24:58 +0530

    chore: Add Autoprefixer for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 const { series, src, dest } = require('gulp');
 const clean = require('gulp-clean');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 function cssClean() {
-    return src('css/**/*.css', {read: false})
-    .pipe(clean());
+    return src('css/**/*.css', { read: false })
+        .pipe(clean());
 }
 
 function cssTranspile(cb) {
     return src("sass/**/*.scss")
-    .pipe(sass()).on("error", sass.logError)
-    .pipe(dest("css"));
+        .pipe(sass()).on("error", sass.logError)
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"]
+            })
+        )
+        .pipe(dest("css"));
 }
 
 exports.cssTranspile = cssTranspile;
